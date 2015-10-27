f6d47bc9b9332e03830040a041c9ea19fa6d0d6a
Paris Polyzos <ppolyzos@gmail.com>
2015-10-27 15:01:26 +0200

    feat(app): reset blobs in your storage account based on file's extension

---
+++ AzStorage.Utilities/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+
+namespace AzStorage.Utilities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["AzureStorage"]?.ConnectionString ?? string.Empty;
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentNullException("AzureStorage ConnectionString not provided");
+            }
+
+            var resetUtil = new BlobReset(connectionString);
+            
+            // resets all blobs in all containers found in your storage account
+            resetUtil.ResetAllContainers(); 
+
+            // or reset all blobs in container named `playlists`
+            //resetUtil.ResetBlobs("playlists");
+        }
+    }
+}
